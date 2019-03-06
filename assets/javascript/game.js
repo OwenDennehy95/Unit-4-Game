3a4e343cafdf486f942d41a700070fb23df0730c
finnias <owen.dennehy@me.com>
2019-03-06 10:13:50 -0500

    cssUpdate

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,7 +12,7 @@ var finalScore;
 function reset() {
    //reset//
    randNum = Math.floor(Math.random() * (120 - 19) + 19);
-   console.log('randomNumber =  ' + randNum);
+   console.log('#randomNumbberGen =  ' + randNum);
    crys1 = Math.floor(Math.random() * (12 - 1) + 1);
    crys2 = Math.floor(Math.random() * (12 - 1) + 1);
    crys3 = Math.floor(Math.random() * (12 - 1) + 1);
@@ -20,53 +20,53 @@ function reset() {
    finalScore = 0;
 
    //HTML//
-   $('.ranNumBox').html(randNum);
-   $('.scoreDisplayChild').html(finalScore);
+   $('#randomNumberGen').html(randNum);
+   $('#winCount').html(finalScore);
    start();
 }
 
 function start() {
    //reset//
    randNum = Math.floor(Math.random() * (120 - 19) + 19);
-   console.log('randomNumber =  ' + randNum);
+   console.log('randomNumberGen =  ' + randNum);
    crys1 = Math.floor(Math.random() * (12 - 1) + 1);
    crys2 = Math.floor(Math.random() * (12 - 1) + 1);
    crys3 = Math.floor(Math.random() * (12 - 1) + 1);
    crys4 = Math.floor(Math.random() * (12 - 1) + 1)
    finalScore = 0;
 
-   $('.ranNumBox').html(randNum);
-   $('.scoreDisplayChild').html(finalScore);
+   $('#randomNumberGen').html(randNum);
+   $('#winCount').html(finalScore);
 
    // random value crystals//
-   $('.crystal1').attr("value", crys1);
-   var test = $('.crystal1').attr("value");
+   $('#crystalB1').attr("value", crys1);
+   var test = $('#crystalB1').attr("value");
 
-   $('.crystal2').attr('value', crys2);
-   var test = $('.crystal2').attr("value");
+   $('#crystalB2').attr('value', crys2);
+   var test = $('#crystalB2').attr("value");
 
-   $('.crystal3').attr("value", crys3);
-   var test = $('.crystal3').attr("value");
+   $('#crystalB3').attr("value", crys3);
+   var test = $('#crystalB3').attr("value");
 
-   $('.crystal4').attr("value", crys4);
-   var test = $('.crystal4').attr("value");
+   $('#crystalB4').attr("value", crys4);
+   var test = $('#crystalB4').attr("value");
 
    //When clicked saves to Final score
    //added the .off() because it will repeat the click twice without it and save the last number that
    // was clicked and push it to the total score during the next game.
-   $('.box').off().on('click', function () {
+   $('.button').off().on('click', function () {
        var test1 = $(this).attr('value');
        //Adds to final score when clicked
        finalScore = parseInt(finalScore) + parseInt(test1);
        console.log('Crystal*  ' + test1);
        console.log('finalScore***  ' + finalScore);
-       $('.scoreDisplayChild').html(finalScore);
+       $('#winCount').html(finalScore);
 
        if (randNum === finalScore) {
            alert('win');
            win++;
-           $('.win').html(win);
-           $('.scoreDisplayChild').html(0);
+           $('.winCount').html(win);
+           $('#winCount').html(0);
            reset();
 
        }
@@ -74,7 +74,7 @@ function start() {
            alert('loser');
            loss++;
            $('.loss').html(loss);
-           $('.scoreDisplayChild').html(0);
+           $('#winCount').html(0);
            reset();
        }
 
